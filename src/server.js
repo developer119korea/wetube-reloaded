24b5e4aea10785467dcc14b7b801ebbfe14a4273
Youngwan Cho <developer119korea@gmail.com>
2021-08-16 16:44:34 +0900

    익스프레스 라우터를 활용하여, 도메인을 처리

--- src/server.js
+++ src/server.js
@@ -1,22 +1,28 @@
 import express from "express";
 import morgan from "morgan";
 
+
 const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
-
-const home = (req, res) => {
-  return res.send("home");
-}
-
-const login = (req, res) => {
-  return res.send("login");
-}
-
 app.use(logger);
-app.get("/", home);
-app.get("/login", login);
+
+const globalRouter = express.Router();
+const handleHome = (req, res) => res.send("Home");
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+const handleEidtUser = (req, res) => res.send("Edit User");
+userRouter.get("/edit", handleEidtUser);
+
+const videoRouter = express.Router();
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/video", videoRouter);
 
 const handleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 
