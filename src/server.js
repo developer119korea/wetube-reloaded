7a91966593baf55d62ce4dc903bb469fed05fa86
Youngwan Cho <developer119korea@gmail.com>
2024-04-07 20:28:47 +0900

    뷰엔진으로 pug를 사용

--- src/server.js
+++ src/server.js
@@ -1,19 +1,24 @@
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
-import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
+
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+
 app.use(logger);
 
 app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/video", videoRouter);
 
-const handleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
+const handleListening = () =>
+  console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
