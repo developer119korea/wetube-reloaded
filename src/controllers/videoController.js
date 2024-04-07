fd3efd753ddaa50f289614af3db549bb6314b5f5
Youngwan Cho <developer119korea@gmail.com>
2024-04-07 20:14:39 +0900

    비디오 라우팅시 HTML를 반환하도록 수정

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,19 @@
-export const trending = (req, res) => res.send("Home Page Videos");
+export const trending = (req, res) =>
+  res.send(
+    "<!DOCTYPE html><html lang='ko'><head><title>Wetube</title></head><body><h1>Home</h1><footer>&copy;2021 Wetube - All rights reserved</footer></body></html>"
+  );
 export const see = (req, res) => {
-  res.send(`Watch Video #${req.params.id}`);
-}
+  res.send(
+    `<!DOCTYPE html><html lang='ko'><head><title>Wetube</title></head><body><h1>Watch video #${req.params.id}</h1><footer>&copy;2021 Wetube -  All rights reserved</footer></body></html>`
+  );
+};
 export const edit = (req, res) => {
-  res.send(`Edit Video #${req.params.id}`);
-}
+  return res.send(
+    `<!DOCTYPE html><html lang='ko'><head><title>Wetube</title></head><body><h1>Edit video #${req.params.id}</h1><footer>&copy;2021 Wetube -  All rights reserved</footer></body></html>`
+  );
+};
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
   res.send(`Delete Video #${req.params.id}`);
-}
+};
